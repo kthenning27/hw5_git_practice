43967417445e9d9ed345bc4c40b4f73e28e55e72
Katie Henning <katiechenning@Katies-MBP-9.attlocal.net>
2021-03-10 19:26:58 -0500

    addin more text

--- hw5_git.py
+++ hw5_git.py
@@ -1,7 +1,5 @@
-print("Hello, World!")
+print("Friends is the best 1990's sitcom of all time.")
 
-print("Frasier is the best 1990's sitcom of all time.")
+print("No, Seinfeld is.")
 
-print("No,  Seinfeld is.")
-
-print("You're both crazy, the best one is Friends.")
+print("You're both crazy, the best one is Frasier.")
